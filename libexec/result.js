082fcf601a50329ffb926f9928001cf0da08c52b
Chris McIntyre <chris@kingsds.network>
2021-11-10 17:00:50 -0500

    Added supplementary result upload service for temporary handling of current supervisor remote results.

---
+++ libexec/result.js
@@ -0,0 +1,33 @@
+/**
+ * @file        libexec/result.js
+ *              /method/result method for dcp-rds-httpd server for
+ *              temporary handling of JSON-formatted remote results
+ * @author      Chris M, chris@kingsds.network
+ * @date        Nov 2021
+ */
+'use strict';
+
+const utils = require('../lib/utils');
+const config = require('../etc/config');    
+
+async function result(request, response, query)
+{
+  var jobId       = query.jobId   || utils.randomId();
+  jobId = JSON.parse(jobId);
+  var element     = query.element || query['slice'] || utils.randomId();
+  element = JSON.parse(element);
+  var elementType = query.elementType || '"result"';
+  elementType = JSON.parse(elementType);
+  var content     = query.content || query[elementType] || null;
+  content = JSON.parse(content);
+  var contentType = query.contentType;
+  content = JSON.parse(contentType);
+  var location = utils.getLocation(request);
+  var href        = `${location.protocol}//${location.host}/methods/download/jobs/${jobId}/${elementType}/${element}`;
+
+  await utils.storeContent(content, contentType, jobId, elementType, element);
+
+  return { success: true, href };
+}
+
+exports.entrypoint = result;
